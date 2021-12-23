b65d13efb4e3edb6f9ddd93aa65000a9b17916e6
glassydan <glassydan@gabuterscape.my.id>
2021-12-23 01:11:40 +0000

    Revert "msm8953-common: Bring back MIUI Charger"
    This reverts commit e317656b218bd4716d8cc309fe3affd129d5a39e.

--- msm8953-common-vendor.mk
+++ msm8953-common-vendor.mk
@@ -650,5 +650,4 @@ PRODUCT_PACKAGES += \
     qcrilmsgtunnel \
     android.hardware.gnss@2.0-service-qti \
     manifest_android.hardware.drm@1.2-service.widevine \
-    vendor.qti.gnss@3.0-service \
-    chargeonlymode
+    vendor.qti.gnss@3.0-service
